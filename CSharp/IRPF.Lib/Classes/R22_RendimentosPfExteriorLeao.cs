e6f027c3e9297d8e1a3a08e5288490fe701bf516
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-03-11 00:58:33 -0300

    Adicionados registros 21, 22 e 23
    Adicionados
    * R21_RendimentosPJ
    * R22_RendimentosPfExteriorLeao
    * R23_RendimentosIsentosNaoTributaveis

---
+++ CSharp/IRPF.Lib/Classes/R22_RendimentosPfExteriorLeao.cs
@@ -0,0 +1,62 @@
+﻿using IRPF.Lib.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRPF.Lib.Classes
+{
+    public class R22_RendimentosPfExteriorLeao : IFixedLenLine
+    {
+        [Index(1), Type(TipoRegistro.N), Length(2)]
+        public int NR_Reg { get; set; }
+
+        [Index(2), Type(TipoRegistro.C), Length(11)]
+        public string NR_Cpf { get; set; }
+
+        [Index(3), Type(TipoRegistro.C), Length(1)]
+        public string E_Dependente { get; set; }
+
+        [Index(4), Type(TipoRegistro.C), Length(11)]
+        public string NR_CpfDepen { get; set; }
+
+        [Index(5), Type(TipoRegistro.N), Length(2)]
+        public int NR_Mes { get; set; }
+
+        [Index(6), Type(TipoRegistro.N), Length(13, 2)]
+        public decimal VR_Rendto { get; set; }
+
+        [Index(7), Type(TipoRegistro.N), Length(13, 2)]
+        public decimal VR_Alugueis { get; set; }
+
+        [Index(8), Type(TipoRegistro.N), Length(13, 2)]
+        public decimal VR_Outros { get; set; }
+
+        [Index(9), Type(TipoRegistro.N), Length(13, 2)]
+        public decimal VR_Exter { get; set; }
+
+        [Index(10), Type(TipoRegistro.N), Length(13, 2)]
+        public decimal VR_LivCaix { get; set; }
+
+        [Index(11), Type(TipoRegistro.N), Length(13, 2)]
+        public decimal VR_Aliment { get; set; }
+
+        [Index(12), Type(TipoRegistro.N), Length(13, 2)]
+        public decimal VR_Deduc { get; set; }
+
+        [Index(13), Type(TipoRegistro.N), Length(13, 2)]
+        public decimal VR_Previd { get; set; }
+
+        [Index(14), Type(TipoRegistro.N), Length(13, 2)]
+        public decimal VR_BaseCalculo { get; set; }
+
+        [Index(15), Type(TipoRegistro.N), Length(13, 2)]
+        public decimal VR_Imposto { get; set; }
+
+        [Index(16), Type(TipoRegistro.N), Length(10)]
+        public int NR_Controle { get; set; }
+
+
+    }
+}
--- CSharp/IRPF.Lib/Files/DEC_Intermediate.cs
+++ CSharp/IRPF.Lib/Files/DEC_Intermediate.cs
@@ -28,6 +28,10 @@ namespace IRPF.Lib.Files
 
         #endregion
 
+        public R21_RendimentosPJ[] RendimentosPJ { get; private set; }
+        public R22_RendimentosPfExteriorLeao[] RendimentosPfExteriorLeao { get; private set; }
+        public R23_RendimentosIsentosNaoTributaveis[] RendimentosIsentosNaoTributaveis { get; private set; }
+
         public static DEC_Intermediate FromFile(string file)
         {
             DEC_Intermediate dec = new DEC_Intermediate();
@@ -39,36 +43,58 @@ namespace IRPF.Lib.Files
             dec.Declarante = new R16_Declarante();
             dec.Declarante.Deserialize(dec.lines[1]);
 
+            var lstR21 = new List<R21_RendimentosPJ>();
+            var lstR22 = new List<R22_RendimentosPfExteriorLeao>();
+            var lstR23 = new List<R23_RendimentosIsentosNaoTributaveis>();
+
             for (int i = 2; i < dec.lines.Length; i++)
             {
-                processaLinha(i, dec);
+                var linha = dec.lines[i];
+                if (linha.Length < 2) continue;
+                
+                switch (linha.Substring(0, 2))
+                {
+                    case "17":
+                        dec.DemaisRendimentosEImpostoPago = new R17_DemaisRendimentosImpostoPago();
+                        dec.DemaisRendimentosEImpostoPago.Deserialize(linha);
+                        continue;
+                    case "18":
+                        dec.TotaisDeclaracaoSimplificada = new R18_TotaisDeclaracaoDescontoSimplificado();
+                        dec.TotaisDeclaracaoSimplificada.Deserialize(linha);
+                        continue;
+
+                    case "19":
+                        dec.Completa = new R19_Completa();
+                        dec.Completa.Deserialize(linha);
+                        continue;
+
+                    case "21":
+                        var pj = new R21_RendimentosPJ();
+                        pj.Deserialize(linha);
+                        lstR21.Add(pj);
+                        continue;
+                    case "22":
+                        var r22 = new R22_RendimentosPfExteriorLeao();
+                        r22.Deserialize(linha);
+                        lstR22.Add(r22);
+                        continue;
+                    case "23":
+                        var r23 = new R23_RendimentosIsentosNaoTributaveis();
+                        r23.Deserialize(linha);
+                        lstR23.Add(r23);
+                        continue;
+                }
             }
+            dec.RendimentosPJ = lstR21.ToArray();
+            dec.RendimentosPfExteriorLeao = lstR22.ToArray();
+            dec.RendimentosIsentosNaoTributaveis = lstR23.ToArray();
 
             return dec;
         }
         private static void processaLinha(int i, DEC_Intermediate dec)
         {
-            var linha = dec.lines[i];
-            if (linha.Length < 2) return;
+           
 
-            switch (linha.Substring(0, 2))
-            {
-                case "17":
-                    dec.DemaisRendimentosEImpostoPago = new R17_DemaisRendimentosImpostoPago();
-                    dec.DemaisRendimentosEImpostoPago.Deserialize(linha);
-                    return;
-                case "18":
-                    dec.TotaisDeclaracaoSimplificada = new R18_TotaisDeclaracaoDescontoSimplificado();
-                    dec.TotaisDeclaracaoSimplificada.Deserialize(linha);
-                    return;
-
-
-                case "19":
-                    dec.Completa = new R19_Completa();
-                    dec.Completa.Deserialize(linha);
-                    return;
-
-            }
         }
     }
 }
