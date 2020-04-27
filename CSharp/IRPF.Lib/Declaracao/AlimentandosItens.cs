1c2809c14a327cb13775f08a4a77551a01b0de0e
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-04-27 00:55:09 -0300

    Serialização de Datas para Dependentes e alimentandos

--- CSharp/IRPF.Lib/Declaracao/AlimentandosItens.cs
+++ CSharp/IRPF.Lib/Declaracao/AlimentandosItens.cs
@@ -12,7 +12,21 @@ namespace IRPF.Lib.Declaracao
         public string Nome { get; set; }
         [XmlAttribute("cpf")]
         public string CPF { get; set; }
+        [XmlIgnore]
         public DateTime DataNascimento { get; set; }
-
+        [XmlAttribute("dtNascimento")]
+        public string XML_DataNascimento
+        {
+            get { return Helpers.ConversoesXML.Datas_Get(DataNascimento); }
+            set { DataNascimento = Helpers.ConversoesXML.Datas_Set(value); }
+        }
+        [XmlIgnore]
+        public bool Residente { get; set; }
+        [XmlAttribute("residente")]
+        public string XML_Residente
+        {
+            get { return Helpers.ConversoesXML.BoolN_Get(Residente); }
+            set { Residente = Helpers.ConversoesXML.BoolN_Set(value); }
+        }
     }
 }
--- CSharp/IRPF.Lib/Declaracao/DependentesItens.cs
+++ CSharp/IRPF.Lib/Declaracao/DependentesItens.cs
@@ -6,14 +6,21 @@ using System.Xml.Serialization;
 
 namespace IRPF.Lib.Declaracao
 {
-   public class DependentesItens : IRPFBase
-   {
-       [XmlAttribute("codigo")]
-       public int Codigo { get; set; }
-       [XmlAttribute("nome")]
-       public string Nome { get; set; }
-       [XmlAttribute("cpf")]
-       public string CPF { get; set; }
-       public DateTime DataNascimento { get; set; }
+    public class DependentesItens : IRPFBase
+    {
+        [XmlAttribute("codigo")]
+        public int Codigo { get; set; }
+        [XmlAttribute("nome")]
+        public string Nome { get; set; }
+        [XmlAttribute("cpf")]
+        public string CPF { get; set; }
+        [XmlIgnore]
+        public DateTime DataNascimento { get; set; }
+        [XmlAttribute("dataNascimento")]
+        public string XML_DataNascimento
+        {
+            get { return Helpers.ConversoesXML.Datas_Get(DataNascimento); }
+            set { DataNascimento = Helpers.ConversoesXML.Datas_Set(value); }
+        }
     }
 }
