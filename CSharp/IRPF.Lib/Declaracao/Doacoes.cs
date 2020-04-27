9def39aba4b3c03b45d4c226ef84a8515ee4f537
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-04-27 01:10:11 -0300

    Update Doacoes.cs
    Serialização de Decimal para Doações

--- CSharp/IRPF.Lib/Declaracao/Doacoes.cs
+++ CSharp/IRPF.Lib/Declaracao/Doacoes.cs
@@ -1,10 +1,17 @@
 ﻿
+using System.Xml.Serialization;
 namespace IRPF.Lib.Declaracao
 {
     public class Doacoes : IRPFBase
     {
-        //[XmlAttribute("totalDoacoes")] // Dá treta com formato PT-BR
+        [XmlIgnore]
         public decimal TotalDoacoes { get; set; }
+        [XmlAttribute("totalDoacoes")] // Dá treta com formato PT-BR
+        public string XML_TotalDoacoes
+        {
+            get { return Helpers.ConversoesXML.Decimal_Get(TotalDoacoes); }
+            set { TotalDoacoes = Helpers.ConversoesXML.Decimal_Set(value); }
+        }
 
         internal static Doacoes importarDec(Identificador ide, Files.DEC_Intermediate dec)
         {
