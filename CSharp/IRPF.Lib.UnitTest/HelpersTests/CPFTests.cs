34323e9ee6e98e2d741d4914415e9ef8b2190f73
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-04-27 22:18:30 -0300

    Adição de Validação de CPF

---
+++ CSharp/IRPF.Lib.UnitTest/HelpersTests/CPFTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IRPF.Lib.UnitTest.HelpersTests
+{
+    [TestClass]
+    public class CPFTests
+    {
+        [TestMethod]
+        public void Helpers_CPF_Valida_CPF()
+        {
+            // Valida CPFs da documentação
+            Assert.IsTrue(Helpers.CPF.ValidaCPF("111.111.110-30"));
+            Assert.IsTrue(Helpers.CPF.ValidaCPF("222.222.223-03"));
+            Assert.IsTrue(Helpers.CPF.ValidaCPF("333.333.334-14"));
+            Assert.IsTrue(Helpers.CPF.ValidaCPF("444.444.445-25"));
+            Assert.IsTrue(Helpers.CPF.ValidaCPF("555.555.556-36"));
+            // Randômicos do 4Devs
+            Assert.IsTrue(Helpers.CPF.ValidaCPF("557.253.420-00"));
+            Assert.IsTrue(Helpers.CPF.ValidaCPF("048.206.360-22"));
+            Assert.IsTrue(Helpers.CPF.ValidaCPF("720.798.750-19"));
+            Assert.IsTrue(Helpers.CPF.ValidaCPF("997.147.020-97"));
+        }
+    }
+}
--- CSharp/IRPF.Lib/Helpers/CPF.cs
+++ CSharp/IRPF.Lib/Helpers/CPF.cs
@@ -19,7 +19,37 @@ namespace IRPF.Lib.Helpers
 
         public static bool ValidaCPF(string CPF)
         {
-            throw new NotImplementedException();
+            if (CPF.Contains("-")) CPF = CPF.Replace("-", "");
+            if (CPF.Contains(".")) CPF = CPF.Replace(".", "");
+
+            if (string.IsNullOrEmpty(CPF)) return false;
+            if (CPF.Length != 11) return false;
+
+            string cpfParte1 = CPF.Substring(0, 9);
+            char dv1 = CPF[9];
+
+            if (calculaParteCpf(cpfParte1) != dv1) return false;
+            
+            string cpfParte2 = CPF.Substring(0, 10);
+            char dv2 = CPF[10];
+
+            return calculaParteCpf(cpfParte2) == dv2;
+        }
+        public static char calculaParteCpf(string str)
+        {
+            int soma = 0;
+            int peso = str.Length + 1;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                char c = str[i];
+                if (!char.IsDigit(c)) throw new ArgumentException("Invalid char");
+
+                soma += (c - '0') * (peso--);
+            }
+            int mod = soma % 11;
+            if (mod < 2) return '0';
+            return (11 - mod).ToString()[0];
         }
     }
 }
