d4926d36db228bcc3746cb8aebcbd918583c6ae2
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-05-10 16:26:24 -0300

    Classe para Extensões

---
+++ CSharp/IRPF.Lib/Helpers/Extensoes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRPF.Lib.Helpers
+{
+    public static class Extensoes
+    {
+        public static T Get<T>(this Dictionary<string, T> dic, string Key, T Default)
+        {
+            if (dic.ContainsKey(Key)) return dic[Key];
+            return Default;
+        }
+        public static T Get<T, K>(this Dictionary<K, T> dic, K Key, T Default)
+        {
+            if (dic.ContainsKey(Key)) return dic[Key];
+            return Default;
+        }
+    }
+}
